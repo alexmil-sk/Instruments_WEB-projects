afbd1cfe0b58dc47f88ce285809586cca969f8bc
Alex <alexmlrdv@gmail.com>
2021-07-06 14:54:52 +0200

    NW-2 Webpack reset

--- HW2_Webpack/src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict ";
-
-import HomeBtn from './assets/img/home.svg';
-import './styles/style.css';
-import './luxon.js';
-import './calcDate.js';
-import './calcTimerDate.js';
-import './calcTimerSec.js';
